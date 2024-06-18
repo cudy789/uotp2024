5f857c135695adc62d838825ed7c014f360c304c
knuts983 <knuts983@umn.edu>
2024-06-18 14:34:07 -0500

    added hello world program to repeatedly move robot forwards, stop

---
+++ hello_world/hello_world.ino
@@ -0,0 +1,44 @@
+//
+// This program will make the line following robot move forwards 
+// for one second, stop for one second, then repeat, forever.
+//
+
+// These are the pins for the motor driver board. There are two pins per wheel, one for the direction, and the 
+// other for speed.
+int mLeftDir = 9;
+int mLeftSpeed = 10;
+int mRightDir = 11;
+int mRightSpeed = 12;
+
+// Setup all of our pins to be outputs.
+// Everything inside the curly braces ({ ... }) of the setup function runs ONCE.
+void setup() {
+  pinMode(mLeftDir, OUTPUT);
+  pinMode(mLeftSpeed, OUTPUT);
+  pinMode(mRightDir, OUTPUT);
+  pinMode(mRightSpeed, OUTPUT);
+}
+
+// Everything inside the curly braces ({ ... }) of the loop function runs FOREVER.
+void loop() {
+
+  // Here is our code to make the rover move. First we need to set the direction for each motor. 
+  // HIGH is forwards, LOW is backwards. Then we set the speed of each motor from 0 - 255. 
+  // 0 is stopped, 255 is full speed, 127 is half speed, etc..
+
+  // STOP the motors, doesn't matter which direction
+  digitalWrite(mLeftDir, HIGH);
+  digitalWrite(mRightDir, HIGH);
+  analogWrite(mLeftSpeed, 0);
+  analogWrite(mRightSpeed, 0);
+
+  delay(1000);
+
+  // START the motors at 50% speed going forwards
+  digitalWrite(mLeftDir, HIGH);
+  digitalWrite(mRightDir, HIGH);
+  analogWrite(mLeftSpeed, 127);
+  analogWrite(mRightSpeed, 127);
+
+  delay(1000);
+}
