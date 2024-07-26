f7aa45e8b21cad0940c9d21f17659bd7672d127f
davidorser <davidorser@gmail.com>
2024-07-26 06:52:04 -0500

    Simplified

--- bt_basic_stretch/bt_basic_stretch.ino
+++ bt_basic_stretch/bt_basic_stretch.ino
@@ -2,146 +2,46 @@
 //   This program will tests all of the lights, sensors, and motors on the line-following robot
 //   to make sure all of the wires are hooked up correctly.
 //   
-// test 
 
 #include <SerialBT.h>
 
 int onboardLedPin = 32;
-int leftBlinkPin = 13;
-int rightBlinkPin = 18;
 
-int irLeft = 4;
-int irRight = 5;
-
-int mLeftDir = 6;
-int mLeftSpeed = 7;
-int mRightDir = 8;
-int mRightSpeed = 9;
-
-int timer = 0;
+String user_input = "empty for now";
 
 void setup() {
   pinMode(onboardLedPin, OUTPUT);
-  pinMode(leftBlinkPin, OUTPUT);
-  pinMode(rightBlinkPin, OUTPUT);
-
-  pinMode(irLeft, INPUT);
-  pinMode(irRight, INPUT);
-
-  pinMode(mLeftDir, OUTPUT);
-  pinMode(mLeftSpeed, OUTPUT);
-  pinMode(mRightDir, OUTPUT);
-  pinMode(mRightSpeed, OUTPUT);
-
+  digitalWrite(onboardLedPin,HIGH);
+  
   SerialBT.begin();
+  // while(!SerialBT.available()){delay(100);}
+  delay(3000);
 
 }
 
-int angry_brightness = 0;
-int setAngryEyes = false;
 void loop() {
-
+  
   if (SerialBT) {
     while (SerialBT.available()) {
-      char c = SerialBT.read();
-      if(c == 'a') {
-        setAngryEyes = true;
-      } else if (c == 'h') {
-        setAngryEyes = false;
-      }
+      user_input = SerialBT.readString(); // Read user input
+      user_input.trim();
+      
+      // Write back to the serial port the character received
+      // this helps with debugging
       SerialBT.print("Received:");
-      SerialBT.write(c);
+      SerialBT.print(user_input);
       SerialBT.println(" ");
     }
   }
-  // Use Timer to make eyes grow brighter for first half second
-  if (setAngryEyes==true) {
-    if (timer < 500){
-      angry_brightness = timer/2;
-    // then grow dimmer for last half second
-    } else{
-      angry_brightness = (1000 - timer)/2;
-    }
-    analogWrite(leftBlinkPin, angry_brightness);
-    analogWrite(rightBlinkPin, angry_brightness);
-  } else {
-    // Blink the left and right LEDs in an alternating sequence every 1 second
-    if (timer < 500){
-      digitalWrite(leftBlinkPin, HIGH);
-      digitalWrite(rightBlinkPin, LOW);
-    } else{
-      digitalWrite(leftBlinkPin, LOW);
-      digitalWrite(rightBlinkPin, HIGH);  
-    }
-  }
-
-
-
-  // Indicator for IR sensors on Pico
-  // If either of the IR proximity sensors detect an object, turn the builtin LED on the Pico on
-  if (digitalRead(irLeft)){
-    digitalWrite(onboardLedPin, HIGH);
-  } else if (digitalRead(irRight)) {
-    digitalWrite(onboardLedPin, HIGH);
-  } else {
-    digitalWrite(onboardLedPin, LOW);
-  }
-
-  // Turning logic:
-  //If left but not right, turn right
-  if (digitalRead(irLeft) && !digitalRead(irRight)){
-    rover_right();
-  //If not left but right, turn left
-  } else if (!digitalRead(irLeft) && digitalRead(irRight)) {
-    rover_left();
-  //Otherwise drive straight
-  } else {
-    rover_staight();
-  }
-
   
-// Old timer driving logic
-//   if (timer < 250){
-//     rover_stop();
-//   } else if (timer >= 250 && timer < 500){
-//     rover_staight();
-//   }
-
-
-  if (timer > 1000){
-    timer = 0;
-  } else{
-    timer++;
+  if(user_input == "r") {
+    digitalWrite(onboardLedPin,HIGH);
+  } else if (user_input == "f") {
+    digitalWrite(onboardLedPin,LOW);
+  } else {
+    
   }
-  delay(1);
 
-}
+  delay(5);
 
-
-void rover_stop() {
-    digitalWrite(mLeftDir, HIGH);
-    digitalWrite(mRightDir, HIGH);
-    analogWrite(mLeftSpeed, 0);
-    analogWrite(mRightSpeed, 0);
-}
-
-void rover_staight() {
-    digitalWrite(mLeftDir, HIGH);
-    digitalWrite(mRightDir, HIGH);
-    analogWrite(mLeftSpeed, 127);
-    analogWrite(mRightSpeed, 127);
-}
-
-void rover_right() {
-    digitalWrite(mLeftDir, LOW);
-    digitalWrite(mRightDir, HIGH);
-    analogWrite(mLeftSpeed, 127);
-    analogWrite(mRightSpeed, 127);
-}
-
-void rover_left() {
-    digitalWrite(mLeftDir, HIGH);
-    digitalWrite(mRightDir, LOW);
-    analogWrite(mLeftSpeed, 127);
-    analogWrite(mRightSpeed, 127);
 }
